8263d2cb34972cd26417928da38e30a6bdda4599
Alejandro <estebanvillegasm95@gmail.com>
2023-03-22 14:31:51 -0700

    changed main

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ from functools import wraps
 import os
 from dotenv import load_dotenv
 
+
 app = Flask(__name__)
+app.app_context()
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 load_dotenv()
