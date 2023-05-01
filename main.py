3eb867fd9acd4d9ac774fd527c8e2031eb5b68f4
Alejandro <estebanvillegasm95@gmail.com>
2023-04-30 21:08:01 -0700

    changed main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ load_dotenv()
 def run_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
-    app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+    app.config['SECRET_KEY'] = 'SECRET_KEY'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     Bootstrap(app)
     return app
